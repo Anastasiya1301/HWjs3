f0cc3a7c7d52b38aa2f06950b6797ff0c26680aa
Anastasiya1301 <dudkoanastasiya1301@gmail.com>
2025-05-14 22:34:17 +0300

    домашка

---
+++ js/index.js
@@ -0,0 +1,25 @@
+//1
+let result;
+result = 5 + 5 + '5';
+console.log(result);
+console.log(typeof result);
+
+
+//2
+const email = 'dudkoanastasiya1301@gmail.com';
+console.log(email.includes("@"));
+console.log(email.length);
+
+
+//3
+const my = "My";
+const name = "Name";
+const is = "Is";
+let fullName = `${my} ${name} ${is} Viktor`;
+console.log(fullName);
+
+
+//4
+const userName = "Александр";
+let payment = "300грн";
+alert(`Дякуємо, ${userName}! До сплати ${payment}`);
